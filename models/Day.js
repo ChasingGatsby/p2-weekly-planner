f3caeb495254ae440a6efd72db8bca35b4ce3631
Gatsbeau <jabriel0ne@gmail.com>
2023-12-04 19:58:12 -0500

    Fixed model instance creation for Day

--- models/Day.js
+++ models/Day.js
@@ -4,39 +4,42 @@ const sequelize = require("../config/connection");
 
 class Day extends Model {}
 
-Day.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  date: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  weekday_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: "weekday",
-      key: "id",
+Day.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    date: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    weekday_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "weekday",
+        key: "id",
+      },
+    },
+    week_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "week",
+        key: "id",
+      },
     },
   },
-  week_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: "week",
-      key: "id",
-    },
-  },
-
-  sequelize,
-  timestamps: false,
-  freezeTableName: true,
-  underscored: true,
-  modelName: "day",
-});
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "day",
+  }
+);
 
 module.exports = Day;
